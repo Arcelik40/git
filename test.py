2e4fa25ce1b3b7efdc27379beb37913249f8dbc5
Cihan Sebzeci <26025617@arcelik.com>
2017-07-07 11:56:44 +0300

    Initial commit

---
+++ test.py
@@ -0,0 +1,16 @@
+import serial, time, sys
+
+control = True;
+arduino = serial.Serial('COM3', 9600, timeout = .1)
+time.sleep(1) # ONEMLI
+
+instruction = sys.argv[1]
+arduino.write(instruction)
+while control:
+    data = arduino.readline()[:-2]
+    if data:
+        if (data == "HALT"):
+            control = False;
+        else:
+         print data
+arduino.close()
